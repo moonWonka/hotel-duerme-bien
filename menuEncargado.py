ce1f5e9b8906243e4644e38469cd3870f332f159
MrMichael0324 <michael.riquelme02@inacapmail.cl>
2022-12-20 12:57:14 -0300

    osidosid

--- menuEncargado.py
+++ menuEncargado.py
@@ -4,4 +4,17 @@ from conexion import Conexion
 
 def menuEncargado():
     os.system('cls')
-    print("""====================Menu Encargado=======================""")
+    print("""====================Menu Encargado=======================
+    
+        1. 
+        2. 
+        3. 
+        4. """)
+
+    opcion = int(input('ingrese una opcion: '))
+
+    while opcion not in [1, 2, 3, 4]:
+        print('ingrese una opcion correcta UwU')
+    
+    if opcion == 1:
+        print("CAT")
